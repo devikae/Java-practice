e23300400d31dafca5105463ab73b16dd133f4cb
ikae <ikaesujeel@gmail.com>
2022-01-22 23:29:59 +0900

    1

---
+++ src/org/opentutorials/javatutorials/helloworld/Helloworld.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.helloworld;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+      System.out.println("Hello world");
+      
+		
+	}
+
+}
